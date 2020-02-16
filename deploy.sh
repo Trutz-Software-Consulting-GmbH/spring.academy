6b5cc3b03efc47fbff36e53b1df17fc02afbbbbf
Christian Trutz <info@christian-trutz.de>
2020-02-16 11:25:00 +0100

    Deployment über die ./deploy.sh Datei

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SPRINGACADEMY_VERSION=$1
+
+jekyll build
+docker build -t ctsc/spring.academy:$SPRINGACADEMY_VERSION .
+docker tag ctsc/spring.academy:$SPRINGACADEMY_VERSION ctsc/spring.academy:latest
+docker push ctsc/spring.academy:$SPRINGACADEMY_VERSION
+docker push ctsc/spring.academy:latest
+kubectl config set-context do-fra1-cluster-k8s-tsc
+cat .release/spring-academy/Chart-template.yaml | sed "s/appVersion: latest/appVersion: $SPRINGACADEMY_VERSION/" > .release/spring-academy/Chart.yaml
+helm upgrade website-test .release/spring-academy
+helm list
