9165822546565889cd5de45cfeaaa0b6c3b1c92f
Christian Trutz <info@christian-trutz.de>
2020-02-16 15:00:58 +0100

    deploy.sh taggt jetzt auch die aktuelle Version in Git

--- deploy.sh
+++ deploy.sh
@@ -11,3 +11,8 @@ kubectl config set-context do-fra1-cluster-k8s-tsc
 cat .release/spring-academy/Chart-template.yaml | sed "s/appVersion: latest/appVersion: $SPRINGACADEMY_VERSION/" > .release/spring-academy/Chart.yaml
 helm upgrade website-test .release/spring-academy
 helm list
+git add .
+git commit -m "Version $SPRINGACADEMY_VERSION"
+git tag $SPRINGACADEMY_VERSION
+git push origin --all
+git push origin --tags
