8de682895a1e3e199c4d574e2e5daa1221fc8abd
Christian Trutz <info@christian-trutz.de>
2020-03-19 04:06:58 +0100

    Version 1.0.0

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ docker push ctsc/spring.academy:$SPRINGACADEMY_VERSION
 docker push ctsc/spring.academy:latest
 kubectl config set-context do-fra1-cluster-k8s-tsc
 cat .release/spring-academy/Chart-template.yaml | sed "s/appVersion: latest/appVersion: $SPRINGACADEMY_VERSION/" > .release/spring-academy/Chart.yaml
-helm upgrade website-test .release/spring-academy
+helm upgrade website .release/spring-academy
 helm list
 git add .
 git commit -m "Version $SPRINGACADEMY_VERSION"
