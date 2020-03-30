702f939f29fb4758c7baa59de12ec047d6c4c3be
Christian Trutz <info@christian-trutz.de>
2020-03-30 07:44:19 +0200

    Disqus und Google Tag Manager nur auf Produktion

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 SPRINGACADEMY_VERSION=$1
 
-bundle exec jekyll build
+JEKYLL_ENV=production bundle exec jekyll build
 docker build -t ctsc/spring.academy:$SPRINGACADEMY_VERSION .
 docker tag ctsc/spring.academy:$SPRINGACADEMY_VERSION ctsc/spring.academy:latest
 docker push ctsc/spring.academy:$SPRINGACADEMY_VERSION
